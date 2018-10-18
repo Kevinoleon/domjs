ecb45dd80899bfa14222390895fed38dddd27ad7
kevin leon <kevinoleonr@hotmail.com>
2018-10-18 18:29:16 -0300

    agrego queryselector

--- app.js
+++ app.js
@@ -6,4 +6,17 @@ console.log(Array.isArray(Array.from(titles)));
 
 Array.from(titles).forEach(function(item){
 	console.log(item);
-})
+});
+
+//const wrap1= $('#wrapper');
+const wrap=document.querySelector('#wrapper');
+
+console.log(wrap);
+
+const lk = document.querySelector('#book-list li:nth-child(2) .name');
+
+console.log(lk);
+
+var arreglo= Array.from(document.querySelectorAll('#book-list li .name'));
+
+console.log(arreglo[1]);
